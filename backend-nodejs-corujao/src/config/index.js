aed5d86ffff746a4d05b850b8c91ef713c7915e2
Karina Gomes <kariinagomes_@gmail.com>
2019-10-06 22:01:54 -0300

    add docker-compose

--- backend-nodejs-corujao/src/config/index.js
+++ backend-nodejs-corujao/src/config/index.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 
 module.exports = {
   connection() {
-    mongoose.connect('mongodb+srv://karina:karinag@backend-tcr1r.mongodb.net/backendcorujao?retryWrites=true&w=majority', {
+    mongoose.connect('mongodb://mongodb:27017/backendcorujao', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     });
+
+    //mongodb atlas
+    //mongoose.connect('mongodb+srv://karina:karinag@backend-tcr1r.mongodb.net/backendcorujao?retryWrites=true&w=majority', {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useFindAndModify: false
+    // });
   }
 }
